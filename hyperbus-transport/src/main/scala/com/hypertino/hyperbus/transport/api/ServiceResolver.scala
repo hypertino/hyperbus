c9e36e863b01521912fa356643fa140a57075a51
Maga Abdurakhmanov <maqdev@gmail.com>
2017-04-25 23:10:41 +0200

    + observable ServiceResolver

--- hyperbus-transport/src/main/scala/com/hypertino/hyperbus/transport/api/ServiceResolver.scala
+++ hyperbus-transport/src/main/scala/com/hypertino/hyperbus/transport/api/ServiceResolver.scala
@@ -1,7 +1,11 @@
 package com.hypertino.hyperbus.transport.api
 
 import monix.eval.Task
+import monix.reactive.Observable
 
 trait ServiceResolver {
-  def lookupService(serviceName: String): Task[ServiceEndpoint]
+  def lookupService(serviceName: String): Task[ServiceEndpoint] = {
+    serviceObservable(serviceName).firstL.map(_.head)
+  }
+  def serviceObservable(serviceName: String): Observable[Seq[ServiceEndpoint]]
 }
--- hyperbus-transport/src/main/scala/com/hypertino/hyperbus/transport/resolvers/PlainResolver.scala
+++ hyperbus-transport/src/main/scala/com/hypertino/hyperbus/transport/resolvers/PlainResolver.scala
@@ -1,14 +1,17 @@
 package com.hypertino.hyperbus.transport.resolvers
 
-import com.hypertino.hyperbus.transport.api.{ServiceEndpoint, ServiceResolver}
-import monix.eval.Task
+import com.hypertino.hyperbus.transport.api.{NoTransportRouteException, ServiceEndpoint, ServiceResolver}
+import monix.reactive.Observable
 
-case class PlainEndpoint(hostname: String) extends ServiceEndpoint {
-  override def port: Option[Int] = None
-}
+case class PlainEndpoint(hostname: String, port: Option[Int]) extends ServiceEndpoint
 
-class PlainResolver extends ServiceResolver {
-  override def lookupService(serviceName: String): Task[ServiceEndpoint] = {
-    Task.now(PlainEndpoint(serviceName))
+class PlainResolver(endpointsMap: Map[String, Seq[PlainEndpoint]]) extends ServiceResolver {
+  def this(serviceName: String, hostname: String, port: Option[Int]) = this(Map(serviceName → Seq(PlainEndpoint(hostname, port))))
+
+  override def serviceObservable(serviceName: String): Observable[Seq[ServiceEndpoint]] = {
+    endpointsMap.get(serviceName) match {
+      case Some(endpoints) ⇒ Observable.now(endpoints)
+      case None ⇒ Observable.raiseError(new NoTransportRouteException(s"Can't resolve service $serviceName"))
+    }
   }
 }
