f16b259fbc5000021c612c23f351f1f074a9b9f2
Maga Abdurakhmanov <maqdev@gmail.com>
2017-06-14 20:16:13 +0300

    - ServiceResolver returns exception if there is no services available

--- hyperbus-transport/src/main/scala/com/hypertino/hyperbus/transport/api/ServiceResolver.scala
+++ hyperbus-transport/src/main/scala/com/hypertino/hyperbus/transport/api/ServiceResolver.scala
@@ -6,7 +6,9 @@ import monix.reactive.Observable
 
 trait ServiceResolver {
   def lookupService(hrl: HRL): Task[ServiceEndpoint] = {
-    serviceObservable(hrl).firstL.map(_.head)
+    serviceObservable(hrl).firstL.map(_.headOption.getOrElse{
+      throw new NoTransportRouteException(s"Can't resolve: $hrl")
+    })
   }
   def serviceObservable(hrl: HRL): Observable[Seq[ServiceEndpoint]]
 }
