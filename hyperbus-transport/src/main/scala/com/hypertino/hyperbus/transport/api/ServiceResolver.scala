26dd585ce2eab9f898d9c56484062d4bcc9c350b
Maga Abdurakhmanov <maqdev@gmail.com>
2017-06-14 13:19:36 +0300

    + Resolver works with schema and service address / hostname

--- hyperbus-transport/src/main/scala/com/hypertino/hyperbus/transport/api/ServiceResolver.scala
+++ hyperbus-transport/src/main/scala/com/hypertino/hyperbus/transport/api/ServiceResolver.scala
@@ -1,11 +1,12 @@
 package com.hypertino.hyperbus.transport.api
 
+import com.hypertino.hyperbus.model.HRL
 import monix.eval.Task
 import monix.reactive.Observable
 
 trait ServiceResolver {
-  def lookupService(serviceName: String): Task[ServiceEndpoint] = {
-    serviceObservable(serviceName).firstL.map(_.head)
+  def lookupService(hrl: HRL): Task[ServiceEndpoint] = {
+    serviceObservable(hrl).firstL.map(_.head)
   }
-  def serviceObservable(serviceName: String): Observable[Seq[ServiceEndpoint]]
+  def serviceObservable(hrl: HRL): Observable[Seq[ServiceEndpoint]]
 }
--- hyperbus-transport/src/main/scala/com/hypertino/hyperbus/transport/resolvers/PlainResolver.scala
+++ hyperbus-transport/src/main/scala/com/hypertino/hyperbus/transport/resolvers/PlainResolver.scala
@@ -1,5 +1,8 @@
 package com.hypertino.hyperbus.transport.resolvers
 
+import java.net.{URI}
+
+import com.hypertino.hyperbus.model.HRL
 import com.hypertino.hyperbus.transport.api.{NoTransportRouteException, ServiceEndpoint, ServiceResolver}
 import monix.reactive.Observable
 
@@ -8,10 +11,12 @@ case class PlainEndpoint(hostname: String, port: Option[Int]) extends ServiceEnd
 class PlainResolver(endpointsMap: Map[String, Seq[PlainEndpoint]]) extends ServiceResolver {
   def this(serviceName: String, hostname: String, port: Option[Int]) = this(Map(serviceName → Seq(PlainEndpoint(hostname, port))))
 
-  override def serviceObservable(serviceName: String): Observable[Seq[ServiceEndpoint]] = {
-    endpointsMap.get(serviceName) match {
+  override def serviceObservable(hrl: HRL): Observable[Seq[ServiceEndpoint]] = {
+    val uri = new URI(hrl.resourceLocator)
+    val service = uri.getScheme + "://" + uri.getAuthority
+    endpointsMap.get(service) match {
       case Some(endpoints) ⇒ Observable.now(endpoints)
-      case None ⇒ Observable.raiseError(new NoTransportRouteException(s"Can't resolve service $serviceName"))
+      case None ⇒ Observable.raiseError(new NoTransportRouteException(s"Can't resolve service for $hrl"))
     }
   }
 }
